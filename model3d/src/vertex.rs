b9a340d48bb3d20752f747a3bd39a6dcbcb260f5
lemoonchild <ncastrom21@gmail.com>
2024-10-12 11:55:05 -0600

    feat: adding zbuffer in framebuffer and main

--- model3d/src/vertex.rs
+++ model3d/src/vertex.rs
@@ -13,31 +13,31 @@ pub struct Vertex {
 
 impl Vertex {
   pub fn new(position: Vec3, normal: Vec3, tex_coords: Vec2) -> Self {
-    Vertex {
-      position,
-      normal,
-      tex_coords,
-      color: Color::black(),
-      transformed_position: position,
-      transformed_normal: normal,
+      Vertex {
+        position,
+        normal,
+        tex_coords,
+        color: Color::black(),
+        transformed_position: position,
+        transformed_normal: normal,
+      }
     }
-  }
 
-  pub fn new_with_color(position: Vec3, color: Color) -> Self {
-    Vertex {
-      position,
-      normal: Vec3::new(0.0, 0.0, 0.0),
-      tex_coords: Vec2::new(0.0, 0.0),
-      color,
-      transformed_position: Vec3::new(0.0, 0.0, 0.0),
-      transformed_normal: Vec3::new(0.0, 0.0, 0.0),
+    pub fn new_with_color(position: Vec3, color: Color) -> Self {
+      Vertex {
+        position,
+        normal: Vec3::new(0.0, 0.0, 0.0),
+        tex_coords: Vec2::new(0.0, 0.0),
+        color,
+        transformed_position: Vec3::new(0.0, 0.0, 0.0),
+        transformed_normal: Vec3::new(0.0, 0.0, 0.0),
+      }
     }
-  }
 
-  pub fn set_transformed(&mut self, position: Vec3, normal: Vec3) {
-    self.transformed_position = position;
-    self.transformed_normal = normal;
-  }
+    pub fn set_transformed(&mut self, position: Vec3, normal: Vec3) {
+      self.transformed_position = position;
+      self.transformed_normal = normal;
+    }
 }
 
 impl Default for Vertex {
