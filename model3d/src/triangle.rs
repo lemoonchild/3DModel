ad8964055481f9d161a4dfd5746fbc036a392220
lemoonchild <ncastrom21@gmail.com>
2024-10-12 11:35:26 -0600

    feat: adding bounding box

--- model3d/src/triangle.rs
+++ model3d/src/triangle.rs
@@ -1,14 +1,24 @@
+use nalgebra_glm::{Vec3, dot};
 use crate::fragment::Fragment;
 use crate::vertex::Vertex;
 use crate::line::line;
 
 pub fn triangle(v1: &Vertex, v2: &Vertex, v3: &Vertex) -> Vec<Fragment> {
     let mut fragments = Vec::new();
-
+  
     // Draw the three sides of the triangle
     fragments.extend(line(v1, v2));
     fragments.extend(line(v2, v3));
     fragments.extend(line(v3, v1));
-
+  
     fragments
+  }
+
+fn calculate_bounding_box(v1: &Vec3, v2: &Vec3, v3: &Vec3) -> (i32, i32, i32, i32) {
+    let min_x = v1.x.min(v2.x).min(v3.x).floor() as i32;
+    let min_y = v1.y.min(v2.y).min(v3.y).floor() as i32;
+    let max_x = v1.x.max(v2.x).max(v3.x).ceil() as i32;
+    let max_y = v1.y.max(v2.y).max(v3.y).ceil() as i32;
+
+    (min_x, min_y, max_x, max_y)
 }
